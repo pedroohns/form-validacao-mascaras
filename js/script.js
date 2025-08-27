78285c8d9dc135aacfa06bd74d198121a21b7d73
Pedro Henrique de Souza Silva <iampedro26@gmail.com>
2025-08-26 21:07:01 -0300

    criação de script para validação de form

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// pega os elementos HTML pelo id
+
+const form = document.getElementById('formulario');
+const nome = document.getElementById('nome');
+const email = document.getElementById('email');
+const telefone = document.getElementById('telefone');
+const cpf = document.getElementById('cpf');
+
